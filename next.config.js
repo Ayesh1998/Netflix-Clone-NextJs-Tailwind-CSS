6a77d01d97fcfc672d7972248a3be585414eabf8
Ayesh1998 <ayeshlak1998@gmail.com>
2022-12-27 21:37:10 +0530

    finished banner component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'image.tmdb.org',
+  //       port: '',
+  //       pathname: '/account123/**',
+  //     },
+  //   ],
+  // },
+  images: {
+    domains: ['image.tmdb.org'],
+  },
 }
