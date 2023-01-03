42558e93acba8d2a295c3a16ace51a6d39badec8
Ayesh1998 <ayeshlak1998@gmail.com>
2023-01-03 09:37:29 +0530

    finished rows and thumbnail components

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -8,6 +8,7 @@ import { Movie } from "../typings";
 interface Props {
   netflixOriginals: Movie[];
 }
+
 const Banner = ({ netflixOriginals }: Props) => {
   const [movie, setMovie] = useState<Movie | null>(null);
 
