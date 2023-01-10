6cf5c84cc832d9072c37dd99d9822c0e05019cfc
Ayesh1998 <ayeshlak1998@gmail.com>
2023-01-10 12:28:48 +0530

    Finished signIn signUp and signOut functionalities with firebase

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,7 +1,34 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form/dist/types";
+import useAuth from "../hooks/useAuth";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
+
+  const [login, setLogin] = useState(false);
+  const {signIn, signUp} = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+    if (login) {
+      await signIn(email, password);
+    } else {
+      await signUp(email, password);
+    }
+  };
+
   return (
     <div
       className="relative flex h-screen w-screen flex-col bg-black md:items-center 
@@ -24,34 +51,64 @@ const Login = () => {
         width={150}
         height={150}
       />
-      <form className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+      >
         <h1 className="text-4xl font-semibold">Sign In</h1>
         <div className="space-y-4">
           <label className="inline-block w-full" htmlFor="email">
             <input
-              className="input"
+              className={`input ${
+                errors.email && "border-b-2 border-orange-500"
+              }`}
               type="email"
               id="email"
               placeholder="Email"
+              {...register("email", { required: true })}
             />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
           <label className="inline-block w-full" htmlFor="password">
             <input
-              className="input"
+              className={`input ${
+                errors.password && "border-b-2 border-orange-500"
+              }`}
               type="password"
               id="password"
               placeholder="Password"
+              {...register("password", {
+                required: true,
+                minLength: 4,
+                maxLength: 60,
+              })}
             />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
 
-        <button className="w-full rounded bg-[#e50914] py-3 font-semibold">
+        <button
+          onClick={() => setLogin(true)}
+          className="w-full rounded bg-[#e50914] py-3 font-semibold"
+        >
           Sign In
         </button>
-        
+
         <div className="text-[gray]">
-          New to Netflix?{' '}
-          <button type="submit" className="text-white hover:underline">
+          New to Netflix?{" "}
+          <button
+            onClick={() => setLogin(false)}
+            type="submit"
+            className="text-white hover:underline"
+          >
             {" "}
             Sign Up now
           </button>
